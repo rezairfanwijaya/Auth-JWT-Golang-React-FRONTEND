e07745bac6d9712c1fa120845595b40cbc4d0396
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-05-22 16:45:17 +0700

    add form signin

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,37 @@
+// * import library
+import React from "react"
+import './Login.css'
+
+
+class Login extends React.Component {
+    render() {
+        return (
+            <>
+                <main className="form-signin">
+                    <form>
+                        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+                        <div className="form-floating">
+                            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                                <label for="floatingInput">Email address</label>
+                        </div>
+
+                        <div className="form-floating">
+                            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                            <label for="floatingPassword">Password</label>
+                        </div>
+
+                        <div className="checkbox mb-3">
+                            <label>
+                                <input type="checkbox" value="remember-me" /> Remember me
+                            </label>
+                        </div>
+                        <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+                        <p className="mt-5 mb-3 text-muted">&copy; 2017–2021 Reza Irfan Wijaya</p>
+                    </form>
+                </main>
+            </>
+        )
+    }
+}
+export default Login
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Login from "../login/Login";
+
+export default function Main ()  {
+    return (
+        <>
+            <Login />
+        </>
+
+    )
+}
